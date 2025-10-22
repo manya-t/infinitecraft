19e68c3cf8037342df8392436f97cb8cfc8495a2
Manya <manya.tyutyunik@gmail.com>
2025-10-22 17:51:31 -0400

    added allowed_hosts

--- infinitecraft/settings.py
+++ infinitecraft/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!#lqi1*f!1b5bk78%c&#ou849-0w*2mbooy(58go54b08-_du^'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-default-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['infinitecraft.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
