ed3854df5fc51be069b152272d2e22c5c05fb2fc
Manya <manya.tyutyunik@gmail.com>
2025-10-25 22:44:12 -0400

    admin console

--- items/admin.py
+++ items/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Item, Transformation
+from .models import Item, ItemPair, Transformation
+
+class ItemAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['simplestWayToMake']
+    search_fields = ['name']
+
+class ItemPairAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['first_input', 'second_input','from_AB_C']
+    search_fields = ['first_input__name', 'second_input__name']
+
+class TransformationAdmin(admin.ModelAdmin):
+    autocomplete_fields = ['input_pair', 'output']
+    search_fields = ['input_pair__first_input__name', 'input_pair__second_input__name', 'output__name']
+
 
 # Register your models here.
-admin.site.register(Item)
-admin.site.register(Transformation)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(ItemPair, ItemPairAdmin)
+admin.site.register(Transformation, TransformationAdmin)
