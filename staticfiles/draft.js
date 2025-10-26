7819fdaf0fd6aff0578fc4175d0da7527964722e
Manya <manya.tyutyunik@gmail.com>
2025-10-25 21:38:22 -0400

    staticfiles

---
+++ staticfiles/draft.js
@@ -0,0 +1,37 @@
+function updateEmoji(name, emoji){
+    var url="http://127.0.0.1:8000/api/addEmoji"
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: name,
+            emoji: emoji
+        }),
+        mode: 'no-cors'
+    })
+}
+
+function upperLevelText(element){
+    child = element.firstChild,
+    texts = [];
+
+    while (child) {
+        if (child.nodeType == 3) {
+            texts.push(child.data);
+        }
+        child = child.nextSibling;
+    }
+    return texts.join("");
+}
+
+document.querySelectorAll(".item").forEach((itemDiv) => {
+    var emojiSpan = itemDiv.querySelector(".item-emoji")
+    updateEmoji(upperLevelText(itemDiv).trim(), emojiSpan.innerText.trim())
+})
+
+document.querySelectorAll(".instance").forEach((itemDiv) => {
+    var emojiSpan = itemDiv.querySelector(".instance-emoji")
+    var textSpan = itemDiv.querySelector(".instance-text")
+    updateEmoji(textSpan.innerText.trim(),emojiSpan.innerText.trim().replace(String.fromCharCode(65039),""))
+})
+
+navigator.clipboard.writeText(document.querySelector(".instance-text").innerText)
---
+++ staticfiles/infinitecraft.js
@@ -0,0 +1,5 @@
+function showEdit(){
+    document.querySelector('#editForm').style.display = 'block';
+    document.querySelector('#editButton').style.display = 'none'
+    
+}
